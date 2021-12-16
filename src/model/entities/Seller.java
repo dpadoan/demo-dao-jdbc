0eff409c8e395be84b28ed966f781adc86f1b33a
Daniel Padoan Junior <padoan@gmail.com>
2021-12-16 19:27:53 +0000

    Department Dao and Seller Dao interfaces

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,9 +1,12 @@
 package model.entities;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String name;
